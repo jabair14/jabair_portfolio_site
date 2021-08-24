561e85b6a7d5512780d555363c2a5d8006555126
Jabair <jabairk@gmail.com>
2021-08-24 15:47:18 -0400

    fixed home screen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import NavBar from './components/NavBar'
 import About from './components/About'
 import Contact from './components/Contact'
 import Home from './components/Home'
+import { useHistory } from 'react-router-dom'
 import { Switch, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
+
+  let history = useHistory()
+  
+  history.push("/home")  
   return (
     <div 
       className="App"
